7d665a596956da3fffb660386394c7e14742fae4
Sangshetty Patil <sangshetty.patil@amd.com>
2025-09-26 19:16:40 -0700

    AVED 2025.1 documentation update

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-# AVED
+<table class="sphinxhide" style="width:100%;">
+  <tr>
+    <td align="center">
+      <picture>
+        <source media="(prefers-color-scheme: dark)" srcset="https://raw.githubusercontent.com/Xilinx/Image-Collateral/main/logo-white-text.png">
+        <img alt="AMD logo" src="https://raw.githubusercontent.com/Xilinx/Image-Collateral/main/xilinx-logo.png" width="30%">
+      </picture>
+    </td>
+  </tr>
+</table>
 
-Full documentation for the ALVEO Versal Example Design can be found at the following link: 
-[xilinx.github.io/AVED/](https://xilinx.github.io/AVED/)
+# AMD Alveo™ Versal™ Example Design (AVED)
+
+View the [full documentation](./docs/index.html) for the Alveo Versal Example Design.
+
+
+<hr class="sphinxhide">
+
+<p class="sphinxhide" align="center"><sub>Copyright © 2023–2025 Advanced Micro Devices, Inc.</sub></p>
+
+<p class="sphinxhide" align="center"><sup><a href="https://www.amd.com/en/corporate/copyright">Terms and Conditions</a></sup></p>
---
+++ docs/amd_v80_gen5x8_25.1_20250821/attachments/1107374331/1107374584.tcl
@@ -0,0 +1,139 @@
+    CONFIG.PS_PMC_CONFIG { \
+      BOOT_MODE                         {Custom} \
+      CLOCK_MODE                        {Custom} \
+      DDR_MEMORY_MODE                   {Custom} \
+      DESIGN_MODE                       {1} \
+      DEVICE_INTEGRITY_MODE             {Custom} \
+      IO_CONFIG_MODE                    {Custom} \
+      PCIE_APERTURES_DUAL_ENABLE        {0} \
+      PCIE_APERTURES_SINGLE_ENABLE      {1} \
+      PMC_BANK_1_IO_STANDARD            {LVCMOS3.3} \
+      PMC_CRP_OSPI_REF_CTRL_FREQMHZ     {101} \
+      PMC_CRP_PL0_REF_CTRL_FREQMHZ      {100} \
+      PMC_CRP_PL1_REF_CTRL_FREQMHZ      {33.3333333} \
+      PMC_CRP_PL2_REF_CTRL_FREQMHZ      {250} \
+      PMC_GLITCH_CONFIG                 {{DEPTH_SENSITIVITY 1} {MIN_PULSE_WIDTH 0.5} {TYPE CUSTOM} {VCC_PMC_VALUE 0.70}} \
+      PMC_GPIO_EMIO_PERIPHERAL_ENABLE   {1} \
+      PMC_MIO11                         {{AUX_IO 0} {DIRECTION in}    {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO12                         {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO13                         {{AUX_IO 0} {DIRECTION inout} {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE Reserved}} \
+      PMC_MIO17                         {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO26                         {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO27                         {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO28                         {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO29                         {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO30                         {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO31                         {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO32                         {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO33                         {{AUX_IO 0} {DIRECTION in}    {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO34                         {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO35                         {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO36                         {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO37                         {{AUX_IO 0} {DIRECTION in}    {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO38                         {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO39                         {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO40                         {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO41                         {{AUX_IO 0} {DIRECTION in}    {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO42                         {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO43                         {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO44                         {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO48                         {{AUX_IO 0} {DIRECTION in}    {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO49                         {{AUX_IO 0} {DIRECTION in}    {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO50                         {{AUX_IO 0} {DIRECTION in}    {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO51                         {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PMC_MIO_EN_FOR_PL_PCIE            {0} \
+      PMC_OSPI_PERIPHERAL               {{ENABLE 1} {IO {PMC_MIO 0 .. 11}} {MODE Single}} \
+      PMC_REF_CLK_FREQMHZ               {33.333333} \
+      PMC_SD0_DATA_TRANSFER_MODE        {8Bit} \
+      PMC_SD0_PERIPHERAL                {{CLK_100_SDR_OTAP_DLY 0x00} {CLK_200_SDR_OTAP_DLY 0x2} {CLK_50_DDR_ITAP_DLY 0x1E} {CLK_50_DDR_OTAP_DLY 0x5} {CLK_50_SDR_ITAP_DLY 0x2C} {CLK_50_SDR_OTAP_DLY 0x5} {ENABLE 1} {IO\
+{PMC_MIO 13 .. 25}}} \
+      PMC_SD0_SLOT_TYPE                 {eMMC} \
+      PMC_USE_PMC_NOC_AXI0              {1} \
+      PS_BANK_2_IO_STANDARD             {LVCMOS3.3} \
+      PS_BOARD_INTERFACE                {Custom} \
+      PS_CRL_CPM_TOPSW_REF_CTRL_FREQMHZ {1000} \
+      PS_GEN_IPI0_ENABLE                {0} \
+      PS_GEN_IPI1_ENABLE                {0} \
+      PS_GEN_IPI2_ENABLE                {0} \
+      PS_GEN_IPI3_ENABLE                {1} \
+      PS_GEN_IPI3_MASTER                {R5_0} \
+      PS_GEN_IPI4_ENABLE                {1} \
+      PS_GEN_IPI4_MASTER                {R5_0} \
+      PS_GEN_IPI5_ENABLE                {1} \
+      PS_GEN_IPI5_MASTER                {R5_1} \
+      PS_GEN_IPI6_ENABLE                {1} \
+      PS_GEN_IPI6_MASTER                {R5_1} \
+      PS_GPIO_EMIO_PERIPHERAL_ENABLE    {0} \
+      PS_I2C0_PERIPHERAL                {{ENABLE 1} {IO {PS_MIO 2 .. 3}}} \
+      PS_I2C1_PERIPHERAL                {{ENABLE 1} {IO {PS_MIO 0 .. 1}}} \
+      PS_IRQ_USAGE                      {{CH0 1} {CH1 0} {CH10 0} {CH11 0} {CH12 0} {CH13 0} {CH14 0} {CH15 0} {CH2 0} {CH3 0} {CH4 0} {CH5 1} {CH6 0} {CH7 0} {CH8 0} {CH9 0}} \
+      PS_KAT_ENABLE                     {0} \
+      PS_MIO10                          {{AUX_IO 0} {DIRECTION in}    {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PS_MIO11                          {{AUX_IO 0} {DIRECTION in}    {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PS_MIO12                          {{AUX_IO 0} {DIRECTION inout} {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE Reserved}} \
+      PS_MIO13                          {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PS_MIO14                          {{AUX_IO 0} {DIRECTION in}    {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PS_MIO18                          {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PS_MIO19                          {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PS_MIO22                          {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PS_MIO23                          {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PS_MIO24                          {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PS_MIO25                          {{AUX_IO 0} {DIRECTION in}    {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PS_MIO4                           {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PS_MIO5                           {{AUX_IO 0} {DIRECTION in}    {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PS_MIO6                           {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PS_MIO7                           {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+      PS_MIO8                           {{AUX_IO 0} {DIRECTION in}    {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE Reserved}} \
+      PS_MIO9                           {{AUX_IO 0} {DIRECTION out}   {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL pullup} {SCHMITT 1} {SLEW slow} {USAGE Reserved}} \
+      PS_M_AXI_LPD_DATA_WIDTH           {32} \
+      PS_NUM_FABRIC_RESETS              {1} \
+      PS_PCIE1_PERIPHERAL_ENABLE        {0} \
+      PS_PCIE2_PERIPHERAL_ENABLE        {1} \
+      PS_PCIE_EP_RESET1_IO              {PMC_MIO 24} \
+      PS_PCIE_EP_RESET2_IO              {PMC_MIO 25} \
+      PS_PCIE_RESET                     {{ENABLE 1}} \
+      PS_PL_CONNECTIVITY_MODE           {Custom} \
+      PS_SPI0 {{GRP_SS0_ENABLE 1}       {GRP_SS0_IO {PS_MIO 15}} {GRP_SS1_ENABLE 0} {GRP_SS1_IO {PMC_MIO 14}} {GRP_SS2_ENABLE 0} {GRP_SS2_IO {PMC_MIO 13}} {PERIPHERAL_ENABLE 1} {PERIPHERAL_IO {PS_MIO 12 .. 17}}}\
+\
+      PS_SPI1 {{GRP_SS0_ENABLE 0}       {GRP_SS0_IO {PS_MIO 9}} {GRP_SS1_ENABLE 0} {GRP_SS1_IO {PS_MIO 8}} {GRP_SS2_ENABLE 0} {GRP_SS2_IO {PS_MIO 7}} {PERIPHERAL_ENABLE 0} {PERIPHERAL_IO {PS_MIO 6 .. 11}}} \
+      PS_TTC0_PERIPHERAL_ENABLE         {1} \
+      PS_TTC1_PERIPHERAL_ENABLE         {1} \
+      PS_TTC2_PERIPHERAL_ENABLE         {1} \
+      PS_TTC3_PERIPHERAL_ENABLE         {1} \
+      PS_UART0_PERIPHERAL               {{ENABLE 1} {IO {PS_MIO 8 .. 9}}} \
+      PS_UART1_PERIPHERAL               {{ENABLE 1} {IO {PS_MIO 20 .. 21}}} \
+      PS_USE_FPD_CCI_NOC                {0} \
+      PS_USE_M_AXI_FPD                  {0} \
+      PS_USE_M_AXI_LPD                  {1} \
+      PS_USE_NOC_LPD_AXI0               {1} \
+      PS_USE_PMCPL_CLK0                 {1} \
+      PS_USE_PMCPL_CLK1                 {1} \
+      PS_USE_PMCPL_CLK2                 {1} \
+      PS_USE_S_AXI_LPD                  {0} \
+      SMON_ALARMS                       {Set_Alarms_On} \
+      SMON_ENABLE_TEMP_AVERAGING        {0} \
+      SMON_MEAS100                      {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 4.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {4 V unipolar}} {NAME VCCO_500} {SUPPLY_NUM 9}} \
+      SMON_MEAS101                      {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 4.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {4 V unipolar}} {NAME VCCO_501} {SUPPLY_NUM 10}} \
+      SMON_MEAS102                      {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 4.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {4 V unipolar}} {NAME VCCO_502} {SUPPLY_NUM 11}} \
+      SMON_MEAS103                      {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 4.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {4 V unipolar}} {NAME VCCO_503} {SUPPLY_NUM 12}} \
+      SMON_MEAS104                      {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 2.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {2 V unipolar}} {NAME VCCO_700} {SUPPLY_NUM 13}} \
+      SMON_MEAS105                      {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 2.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {2 V unipolar}} {NAME VCCO_701} {SUPPLY_NUM 14}} \
+      SMON_MEAS106                      {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 2.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {2 V unipolar}} {NAME VCCO_702} {SUPPLY_NUM 15}} \
+      SMON_MEAS118                      {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 2.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {2 V unipolar}} {NAME VCC_PMC} {SUPPLY_NUM 0}} \
+      SMON_MEAS119                      {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 2.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {2 V unipolar}} {NAME VCC_PSFP} {SUPPLY_NUM 1}} \
+      SMON_MEAS120                      {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 2.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {2 V unipolar}} {NAME VCC_PSLP} {SUPPLY_NUM 2}} \
+      SMON_MEAS121                      {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 2.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {2 V unipolar}} {NAME VCC_RAM} {SUPPLY_NUM 3}} \
+      SMON_MEAS122                      {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 2.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {2 V unipolar}} {NAME VCC_SOC} {SUPPLY_NUM 4}} \
+      SMON_MEAS45                       {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 2.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {2 V unipolar}} {NAME GTYP_AVCCAUX_102} {SUPPLY_NUM 20}} \
+      SMON_MEAS46                       {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 2.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {2 V unipolar}} {NAME GTYP_AVCCAUX_103} {SUPPLY_NUM 21}} \
+      SMON_MEAS62                       {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 2.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {2 V unipolar}} {NAME GTYP_AVCC_102} {SUPPLY_NUM 18}} \
+      SMON_MEAS63                       {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 2.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {2 V unipolar}} {NAME GTYP_AVCC_103} {SUPPLY_NUM 19}} \
+      SMON_MEAS79                       {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 2.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {2 V unipolar}} {NAME GTYP_AVTT_102} {SUPPLY_NUM 16}} \
+      SMON_MEAS80                       {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 2.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {2 V unipolar}} {NAME GTYP_AVTT_103} {SUPPLY_NUM 17}} \
+      SMON_MEAS96                       {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 2.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {2 V unipolar}} {NAME VCCAUX} {SUPPLY_NUM 6}} \
+      SMON_MEAS97                       {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 2.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {2 V unipolar}} {NAME VCCAUX_PMC} {SUPPLY_NUM 7}} \
+      SMON_MEAS98                       {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 2.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {2 V unipolar}} {NAME VCCAUX_SMON} {SUPPLY_NUM 8}} \
+      SMON_MEAS99                       {{ALARM_ENABLE 1} {ALARM_LOWER 0.00} {ALARM_UPPER 2.00} {AVERAGE_EN 0} {ENABLE 1} {MODE {2 V unipolar}} {NAME VCCINT} {SUPPLY_NUM 5}} \
+      SMON_TEMP_AVERAGING_SAMPLES       {0} \
+      SMON_VOLTAGE_AVERAGING_SAMPLES    {8} \
+    } \
